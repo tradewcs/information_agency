01ca0d6f3a072c2904e58b2e214b31fcd13c892d
Mike <tradewcs@gmail.com>
2025-12-23 14:31:39 +0200

    fix: change default settings to prod

--- information_agency/asgi.py
+++ information_agency/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "information_agency.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "information_agency.settings.prod")
 
 application = get_asgi_application()
--- information_agency/wsgi.py
+++ information_agency/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "information_agency.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "information_agency.settings.prod")
 
 application = get_wsgi_application()
