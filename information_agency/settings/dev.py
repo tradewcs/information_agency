5dda302c63d77fc36020a4b66360ca345770982d
Mike <tradewcs@gmail.com>
2026-01-04 11:02:26 +0200

    remove

--- information_agency/settings/dev.py
+++ information_agency/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "information-agency-pb3c.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 DATABASES = {
     "default": {
--- information_agency/settings/prod.py
+++ information_agency/settings/prod.py
@@ -4,7 +4,7 @@ import dj_database_url
 from .base import *
 
 
-DEBUG = False
+DEBUG = True
 
 RENDER_DOMAIN = os.environ.get("RENDER_DOMAIN")
 
