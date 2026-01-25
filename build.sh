03e94476a6713be1e76aff51bc348779a9658a99
Mike <tradewcs@gmail.com>
2026-01-25 10:06:15 +0200

    fix migrations issue

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ export DJANGO_SETTINGS_MODULE=information_agency.settings.prod
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py migrate account zero --fake
 python manage.py migrate
 python manage.py loaddata data.json
