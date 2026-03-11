bc7e0ad49e38dd551ac4306a96aced54ce619ef9
Mike <tradewcs@gmail.com>
2026-03-11 11:53:15 +0200

    remove debug info

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-ls /var
 pip install -r requirements.txt
 python manage.py migrate
--- temp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class TempModel(models.Model):
-    name = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
